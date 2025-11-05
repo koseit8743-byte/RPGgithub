2b2310b20558c31c3e670057868c9054756e37c6
DeRaffaele <0945992@my.scccd.edu>
2025-11-05 13:59:24 -0800

    startup

--- main.cc
+++ main.cc
@@ -1,6 +1,26 @@
 //Fill out this comment with your names and which bullet points you did
-//Partners: Lucca DeRaffaele: Puzzels
-//Bullet Points: 
-//Extra Credit: 
+//Partners: DeRaffaele, Romero, Rangel, Osei Tutu,
+//Bullet Points: 1,2,3,4,5
+//Extra Credit: 2,3
 //URL to cover art and music: 
 
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include "/public/colors.h"
+using namespace std;
+
+//START
+
+
+
+
+
+int main() {
+
+
+
+
+
+return 0;
+}
